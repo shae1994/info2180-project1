197413b0094347091b08864613051c43382d7914
shae1994 <juddyshaew@gmail.com>
2021-10-24 21:24:51 -0400

    Updated JS

--- scripts.js
+++ scripts.js
@@ -1,27 +1,33 @@
 $(document).ready(function () {
     console.log("True")
     // Listen to submit event on the <form> itself!
-    $("#submit").submit(function(e) {
+    $("form").submit(function(e) {
         e.preventDefault();
-    });
-
-    var email = $('#email').val();
+        var email = $('#email').val();
 
     if(email== ''){
         $('#email').next().show();
+        console.log("False")
         return false;
+        
     }
     if(Email(email)==false){
-        $('#invalid_email').show();
+        $('#message').show();
+        console.log("False")
         return false;
     }
       function Email(email) {
         var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         if(!regex.test(email)) {
+            console.log("False")
           return false;
         }else{
+            console.log("True")
           return true;
         }
       }
+    });
+
+    
   });
 
